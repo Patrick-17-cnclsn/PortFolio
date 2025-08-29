883f7fe272e60cff95bf2a5e808694e00748b9e9
hassa <offoumoupatrick0@gmail.com>
2025-08-29 20:04:34 +0200

    first portfolio

--- app/components/Portfolio.tsx
+++ app/components/Portfolio.tsx
@@ -10,14 +10,14 @@ const baseProjects = [
         id: 1,
         year: 2025,
         title: "Project 1",
-        description: "",
+        description: "basic-react-essentiel",
         image: "/assets_perso/react_essentiel.png",
     },
     {
         id: 2,
         year: 2025,
         title: "Project 2",
-        description: "T",
+        description: "calculator-invest-basics",
         image: "/assets_perso/invest_react.png",
     },
 
